a6e99032ecdd7a374ecf38218b871f45c9b902ef
Alex Woods <alexhwoods@gmail.com>
2022-10-31 09:53:12 -0400

    feat: establish connection

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
